361ebf0964eb16be5b36d534d7ea15abee843a15
tzurio00 <riorivaldo2000@gmail.com>
2022-11-01 08:41:00 +0700

    Add menu Otp, login

--- bot_mitra_indogrosir.js
+++ bot_mitra_indogrosir.js
@@ -1,6 +1,5 @@
 const { Telegraf, session } = require('telegraf');
 
-const jwt = require('jsonwebtoken');
 const axios = require('axios');
 const path = require("path");
 const dotenv = require('dotenv').config({
@@ -10,56 +9,80 @@ const dotenv = require('dotenv').config({
 const url = process.env.URL;
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-let user_token = null;
+let user_id,user_token, email = null;
 
 bot.start((ctx) => {
     ctx.reply(`Silahkan input email dengan command '/email'`)
 })
 
 bot.command('email', async (ctx) => {
+  try{
     let format =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    let email = ctx.message.text.slice(7).replace(/\s/g, '');
+    email = ctx.message.text.slice(7).replace(/\s/g, '');
+
     if(!email.match(format)){
       return ctx.reply(`Format email tidak valid!`);
     }     
     
-    await ctx.reply(`Silahkan input password dengan command '/password'`);
-
-    bot.command('password', async (ctx) => {
-      let password = (ctx.message.text.slice(9)).replace(/\s/g, '');
-    
-      let payload = { email, password};
-
-      let res = await axios.post(url + '/api/otp/user', payload);
-      let data = res.data;
-
-      user_token = data.token;
-
-      console.log(data);
-
-    });
+    ctx.reply(`Silahkan input password dengan command '/password'`);
+  }catch(err){
+    console.log(err)
+  }
 });
 
-bot.command('menu1', async(ctx) =>{
-  let res = await axios.get(url + '/api/otp/generate/1', { 
-    headers: {
-      'Authorization' : user_token
-    }});
+bot.command('password', async (ctx) => {
+  try{
 
-  console.log(res);
+    let password = (ctx.message.text.slice(9)).replace(/\s/g, '');
 
-  let data = res.data;
+    let payload = { email, password};
+  
+    let res = await axios.post(`${url}/api/otp/user`, payload);
+    let data = res.data;
+  
+    if(!data){
+      return ctx.reply(`Username atau password anda salah!`)
+    }
 
-  // console.log(data);
+    user_id = data.user.id
+    user_token = data.token;
+
+    console.log(data);
+  }catch(err){
+    console.log(err);
+    if(err.response.status == 401){
+      return ctx.reply(`Akun anda unauthorized!`);
+    }
+  }
 });
 
-bot.command('quit', async (ctx) => {
-    // Explicit usage
-    await ctx.telegram.leaveChat(ctx.message.chat.id);
+bot.command('menu1', async(ctx) =>{ 
+  try{
+    console.log(typeof(user_id))
+    console.log(user_id);
+    console.log(user_token)
 
-    // Using context shortcut
-    await ctx.leaveChat();
+    if(!user_token){
+      return ctx.reply(`Anda belum melakukan login ke Bot Mitra Indogrosir!`);
+    }
+  
+    let res = await axios.get(`${url}/api/otp/generate/${user_id}`, { 
+      headers: {
+        'Authorization' : user_token.toString()
+      }});
+
+    console.log(res);
+  
+    let data = res.data;
+  
+    await ctx.reply(`OTP Anda : ${data.otp} \nSilahkan gunakan OTP pada CMS Mitra Indogrosir!`);
+  }catch(err){
+    console.log(err);
+    if(err.response.status == 401){
+      return ctx.reply(`Akun anda unauthorized!`);
+    }
+  }
 });
 
 bot.launch();
